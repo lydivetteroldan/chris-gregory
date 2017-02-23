8dbc023dea49000d963ccaf2fbe10ea26ac33a7c
lydivetteroldan <sofiastefanich@gmail.com>
2017-02-23 17:58:49 -0400

    Add Wordpress files

---
+++ wp-content/themes/chris-gregory/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Chris_Gregory
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'chris-gregory' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'chris-gregory' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'chris-gregory' ), 'chris-gregory', '<a href="https://automattic.com/" rel="designer">Lydivette Roldán</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
