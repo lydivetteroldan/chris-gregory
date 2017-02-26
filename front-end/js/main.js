87a46b13ef4a0cd987cc39d80e54bf9ac3c618ea
lydivetteroldan <sofiastefanich@gmail.com>
2017-02-26 13:06:04 -0400

    Add main.js with Stellar init code

---
+++ front-end/js/main.js
@@ -0,0 +1,9 @@
+$("#main").stellar({
+  horizontalScrolling: false,
+  verticalOffset: -100,
+  scrollProperty: 'transform',
+  responsive: false,
+  positionProperty: 'transform',
+  parallaxBackgrounds: false,
+  hideDistantElements: false
+});
