a8b611af1a3f27cc69b2f10ea2fbd2764884c983
lydivetteroldan <sofiastefanich@gmail.com>
2017-02-27 17:26:34 -0400

    Add shame :(

--- front-end/js/main.js
+++
@@ -1,9 +0,0 @@
-$("#main").stellar({
-  horizontalScrolling: false,
-  verticalOffset: -100,
-  scrollProperty: 'transform',
-  responsive: false,
-  positionProperty: 'transform',
-  parallaxBackgrounds: false,
-  hideDistantElements: false
-});
