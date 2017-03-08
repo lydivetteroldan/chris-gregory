ecc34e0392661be39227f11e867f45ce32786501
lydivetteroldan <sofiastefanich@gmail.com>
2017-03-07 20:08:05 -0400

    Add Projects Page temporary template

---
+++ wp-content/themes/chris-gregory/page-projects.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: Projects Page
+ *
+ * This is the template that displays the Projects page.
+ *
+ * @package Chris_Gregory
+ */
+
+get_header(); ?>
+
+	<div class="content-area" id="primary">
+		<main class="main" id="main" role="main">
+			<div class="container page-container layout--3col">
+				<div class="row">
+					<div class="page-row">
+						<?php
+						global $post;
+						$args = array( 'order'=> 'ASC', 'post_type' => 'owl-carousel', 'post_status' => 'publish' );
+						$postslist = get_posts( $args );
+						foreach ( $postslist as $post ) :
+						  setup_postdata( $post ); ?> 
+							<div class="col-sm-4 col-md-4">
+								<div class="page-thumbnail">
+									<a href="<?php the_permalink(); ?>">
+										<?php the_post_thumbnail() ?>
+										<span class="page-caption">
+											<?php the_title(); ?>
+										</span>
+									</a>
+								</div>
+							</div>
+						<?php
+						endforeach; 
+						wp_reset_postdata();
+						?>
+					</div>
+				</div>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
