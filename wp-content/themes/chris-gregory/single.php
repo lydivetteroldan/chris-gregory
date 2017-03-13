aad0c7681ad45bcd96d82a2ba368eb99c743c3f5
lydivetteroldan <sofiastefanich@gmail.com>
2017-03-12 21:38:26 -0400

    Add responsive container to single post template

--- wp-content/themes/chris-gregory/single.php
+++ wp-content/themes/chris-gregory/single.php
@@ -10,23 +10,18 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		while ( have_posts() ) : the_post();
-
-			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+		<main class="main front-gallery" id="main" role="main">
+			<div class="container-fluid page-container">
+				<div class="row">
+					<div class="col-md-12">
+						<?php
+						while ( have_posts() ) : the_post();
+							get_template_part( 'template-parts/content', get_post_format() );
+						endwhile; // End of the loop.
+						?>
+					</div>
+				</div>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
