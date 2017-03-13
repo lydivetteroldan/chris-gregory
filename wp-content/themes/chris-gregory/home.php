4fde3fef9b06c90fad99ddf713e217b6442bfa02
lydivetteroldan <sofiastefanich@gmail.com>
2017-03-12 21:38:58 -0400

    Add Front Page Gallery template with placeholder images

--- wp-content/themes/chris-gregory/home.php
+++ wp-content/themes/chris-gregory/home.php
@@ -10,23 +10,83 @@
 get_header(); ?>
 
 	<div class="content-area" id="primary">
-		<main class="main" id="main" role="main">
+		<main class="main front-gallery" id="main" role="main">
 			<div class="container-fluid page-container">
-				<?php
-				global $post;
-				$args = array(
-					'posts_per_page'   => 11,
-					'orderby'          => 'date',
-					'order'            => 'ASC',
-					'post_type'        => 'front_page_image',
-					'post_status'      => 'publish'
-				);
-				$lastposts = get_posts( $args );
-				foreach ( $lastposts as $post ) :
-				  setup_postdata( $post ); ?>
-					<?php the_content(); ?>
-				<?php endforeach; 
-				wp_reset_postdata(); ?>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="row">
+							<?php
+							global $post;
+							$args = array(
+								'posts_per_page'   => 11,
+								'orderby'          => 'date',
+								'order'            => 'ASC',
+								'post_type'        => 'front_page_image',
+								'post_status'      => 'publish'
+							);
+							$lastposts = get_posts( $args );
+							foreach ( $lastposts as $post ) :
+							  setup_postdata( $post ); ?>
+								<?php if (has_term('right-portrait-small', 'image_format') ) : ?>
+								<div class="col-md-4 project-image image--right image--portrait image--sm" data-stellar-ratio="0.5">
+									<?php the_content(); ?>
+								</div><!-- image right portrait small -->
+
+								<?php elseif (has_term('faster', 'image_format') ) : ?>
+								<div class="col-md-5 project-image image--left image--portrait image--md z2" data-stellar-ratio="2.5">
+									<?php the_content(); ?>
+								</div><!-- image left portrait medium-->
+
+								<?php elseif (has_term('right-landscape-large', 'image_format') ) : ?>
+								<div class="col-md-8 project-image image--right image--landscape image--lg" data-stellar-ratio="1">
+									<?php the_content(); ?>
+								</div><!-- image right landscape large-->
+
+								<?php elseif (has_term('center-portrait', 'image_format') ) : ?>
+								<div class="col-md-4 col-lg-3 col-md-offset-5 project-image image--center image--portrait z4" data-stellar-ratio="1">
+									<?php the_content(); ?>
+								</div><!-- image center potrait -->
+
+								<?php elseif (has_term('left-landscape-medium', 'image_format') ) : ?>
+								<div class="col-md-6 project-image image--left image--landscape image--md z3" data-stellar-ratio="1">
+									<?php the_content(); ?>
+								</div><!-- image left landscape medium -->
+
+								<?php elseif (has_term('right-portrait-large', 'image_format') ) : ?>
+								<div class="col-md-8 project-image image--right image--portrait image--lg z2" data-stellar-ratio="1.2">
+									<?php the_content(); ?>
+								</div><!-- image right portrait large-->
+
+								<?php elseif (has_term('overlapped', 'image_format') ) : ?>
+								<div class="col-md-5 project-image image--left image--portrait image--md offset--top" data-stellar-ratio="1.1">
+									<?php the_content(); ?>
+								</div><!-- image left portrait medium-->
+
+								<?php elseif (has_term('center-landscape', 'image_format') ) : ?>
+								<div class="col-md-5 col-md-offset-5 project-image image--center image--landscape z4" data-stellar-ratio="1.1">
+									<?php the_content(); ?>
+								</div><!-- image center landscape-->
+
+								<?php elseif (has_term('left-portrait-medium', 'image_format') ) : ?>
+								<div class="col-md-5 project-image image--left image--portrait image--md z3 offset--top" data-stellar-ratio="1">
+										<?php the_content(); ?>
+								</div><!-- image left portrait medium-->
+
+								<?php elseif (has_term('right-landscape-medium', 'image_format') ) : ?>
+								<div class="col-md-7 project-image image--right image--landscape image--md z2" data-stellar-ratio="1">
+										<?php the_content(); ?>
+								</div><!-- image right landscape medium-->
+
+								<?php elseif (has_term('static', 'image_format') ) : ?>
+								<div class="col-md-8 project-image image--left image--landscape image--lg">
+									<?php the_content(); ?>
+								</div><!-- image left landscape medium -->
+								<?php endif ?>
+							<?php endforeach; 
+							wp_reset_postdata(); ?>
+						</div>
+					</div>
+				</div>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
