4b2347213daeada0603223ba6685bdcac8c2a518
lydivetteroldan <sofiastefanich@gmail.com>
2017-04-22 15:45:38 -0400

    Include social links URL and replace dashes with dots in phone number

--- wp-content/themes/chris-gregory/sidebar.php
+++ wp-content/themes/chris-gregory/sidebar.php
@@ -15,8 +15,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <aside class="col-md-3 col-md-offset-1" id="secondary" role="complementary">
   <ul class="list-unstyled page-list">
     <li><a href="mailto:chris@chrisgregoryphoto.com" target="_blank">chris@chrisgregoryphoto.com</a></li>
-    <li><a href="tel:7872429215" target="_blank">787-242-9215</a></li>
-    <li><a href="https://instagram.com/" target="_blank">Instagram</a></li>
-    <li><a href="https://facebook.com" target="_blank">Facebook</a></li>
+    <li><a href="tel:7872429215" target="_blank">787.242.9215</a></li>
+    <li><a href="https://instagram.com/cgregoryphoto" target="_blank">Instagram</a></li>
+    <li><a href="https://www.facebook.com/cgregorypr" target="_blank">Facebook</a></li>
   </ul>
 </aside><!-- #secondary -->
